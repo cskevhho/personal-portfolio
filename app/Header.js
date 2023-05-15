09c2c2e40d33fc2c01542cf2c596d2e86d7294ac
Kevin Ho <kevhho@my.yorku.ca>
2023-05-15 11:20:08 -0400

    working commit

---
+++ app/Header.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <p><h1>Hi there! 👋</h1></p>
+    </header>
+  )
+}
